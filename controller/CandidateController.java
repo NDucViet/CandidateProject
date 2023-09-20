148f4c875156a8cece2ae5c4a9e7b594dbb62359
NDucViet <vietnguyen4002@gmail.com>
2023-09-20 08:51:14 +0700

    CandidateProject

---
+++ controller/CandidateController.java
@@ -0,0 +1,42 @@
+package CandidateProject.controller;
+
+import java.util.Scanner;
+
+import CandidateProject.view.Menu;
+
+public class CandidateController extends Menu {
+    Scanner sc = new Scanner(System.in);
+    static String[] mc = { "Experience", "Fresher", "Internship", "Searching", "Exit" };
+    Library library = new Library();
+    Algorithm algorithm = new Algorithm();
+
+    public CandidateController() {
+        super("Programig", mc);
+    }
+
+    public void execute(int n) {
+        
+            switch (n) {
+                case 1:
+                    algorithm.createCandidate(n - 1);
+                    break;
+                case 2:
+                    algorithm.createCandidate(n - 1);
+                    break;
+                case 3:
+                    algorithm.createCandidate(n - 1);
+                    break;
+                case 4:
+                    algorithm.searchCandidate();
+                    break;
+                case 5:
+                    
+                    break;
+
+            }
+
+        
+
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+package CandidateProject;
+
+import CandidateProject.controller.CandidateController;
+
+public class main {
+    public static void main (String[] args){
+       CandidateController candidateController = new CandidateController();
+       candidateController.run();
+    }
+}
